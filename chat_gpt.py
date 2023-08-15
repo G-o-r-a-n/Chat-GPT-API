688bd369a019c5979b0ea3aa32e3d7f25d4aa329
Goran Hourami <github@hourami.co>
2023-08-16 00:40:40 +0100

    Updated formatting

--- chat_gpt.py
+++ chat_gpt.py
@@ -1,6 +1,8 @@
 import os
 import openai
+from dotenv import load_dotenv
 
+load_dotenv()
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
 messages = []
@@ -10,17 +12,17 @@ assistant_init = {"role": "system", "content": assistant_type}
 messages.append(assistant_init)
 
 while True:
-    user_query = input("Enter your query: ")
+    user_query = input("\n\nEnter your query: ")
     if user_query:
         user_input = {"role": "user", "content": user_query}
         messages.append(user_input)
         chat = openai.ChatCompletion.create(
             model="gpt-3.5-turbo",
             messages=messages,
-            temperature=0.4
+            temperature=0.5
             )
 
     output = chat.choices[0].message.content
     assistant_output = {"role": "assistant", "content": output}
-    print(f"Chat GPT: {output}")
+    print(f"\n\nChat GPT: {output}")
     messages.append(assistant_output)
